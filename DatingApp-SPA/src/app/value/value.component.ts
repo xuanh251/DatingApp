4e7d038391a845564f49a2fe24976008f2268bf8
xuanh251 <lexuanhoang1997@gmail.com>
2019-01-03 11:25:42 +0700

    init

---
+++ DatingApp-SPA/src/app/value/value.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Component({
+  selector: 'app-value',
+  templateUrl: './value.component.html',
+  styleUrls: ['./value.component.scss']
+})
+export class ValueComponent implements OnInit {
+  // bien nay de lay gia tri ra ngoai
+  value: any;
+
+  constructor(private http: HttpClient) { }
+
+  ngOnInit() {
+    this.getValues();
+  }
+  getValues() {
+    this.http.get('http://localhost:5000/api/values').subscribe(
+      res => {
+        this.value = res;
+      },
+      err => {
+        console.log(err);
+      }
+    );
+  }
+}
---
+++ DatingApp.API/Controllers/ValuesController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DatingApp.API.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace DatingApp.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        private readonly DataContext _context;
+        public ValuesController(DataContext context){
+            _context = context;
+        }
+        // GET api/values
+        [HttpGet]
+        public async Task<ActionResult> GetValues()
+        {
+            var rs = await _context.Values.ToListAsync();
+            return Ok(rs);
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult> GetValue(int id)
+        {
+            var rs = await _context.Values.FirstOrDefaultAsync(s=>s.Id==id);
+            return Ok(rs);
+        }
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
